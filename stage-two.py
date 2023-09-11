c0314f2f916cbd6193d3409b91b36614cc7efcbb
Veronfc <veronfc1701@gmail.com>
2023-09-11 15:24:06 +0200

    start main window

--- stage-two.py
+++ stage-two.py
@@ -1,6 +1,10 @@
+#GUI imports
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import *
+from tkinter import filedialog as fd
+
+#functional imports
 import csv
 
 #WINDOW
@@ -11,22 +15,26 @@ style = ttk.Style()
 window.title('Music Identification')
 
 #center window
+#**************************************************
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
  
-x = (screen_width/2) - (300/2)
-y = (screen_height/2) - (300/2)
+x = (screen_width/2) - (750/2)
+y = (screen_height/2) - (400/2)
  
-window.geometry('%dx%d+%d+%d' % (300, 300, x, y))
+window.geometry('%dx%d+%d+%d' % (750, 400, x, y))
+#**************************************************
 
 #creates frames for each screen
-start = Frame(window)
-main = Frame(window)
+start_frame = Frame(window)
+main_frame = Frame(window)
 
-start.pack(fill=BOTH, expand=True)
+#**************************************************
+main_frame.pack(fill=BOTH, expand=True)
+#**************************************************
 
 #creates tab interface
-tabs = ttk.Notebook(start)
+tabs = ttk.Notebook(start_frame)
 tabs.pack(fill=BOTH, expand=True)
 
 #create a tab for each screen
@@ -37,7 +45,6 @@ signup = Frame(tabs, bg='#DDE6ED')
 tabs.add(login, text='Login')
 tabs.add(signup, text='Sign up')
 ##################################################
-
 
 #LOGIN
 ##################################################
@@ -59,12 +66,12 @@ def check_login():
     for row in reader:
       if login_eusername.get() == row['USERNAME']:
         if login_epassword.get() == row['PASSWORD']:
-          main.pack(fill=BOTH, expand=True)
-          x = (screen_width/2) - (500/2)
-          y = (screen_height/2) - (300/2)
-          window.geometry('%dx%d+%d+%d' % (500, 300, x, y))
+          main_frame.pack(fill=BOTH, expand=True)
+          x = (screen_width/2) - (750/2)
+          y = (screen_height/2) - (400/2)
+          window.geometry('%dx%d+%d+%d' % (750, 400, x, y))
 
-          start.pack_forget()
+          start_frame.pack_forget()
           login_error.config(text='')
         else:
           login_error.config(text='Incorrect password.')
@@ -80,15 +87,15 @@ login_button = ttk.Button(login, text='Log in', style='button.TButton', command=
 style.configure('button.TButton', font=('Ubuntu', 11), justify=CENTER)
 
 #checkbox
-checked = tk.IntVar()
+login_checked = tk.IntVar()
 
 def login_showpassword():
-  if checked.get():
+  if login_checked.get():
     login_epassword.config(show='')
   else:
     login_epassword.config(show='*')
 
-login_checkbox = Checkbutton(login, command=login_showpassword, variable=checked, onvalue=1, offvalue=0, cursor='pencil', bg='#DDE6ED')
+login_checkbox = Checkbutton(login, command=login_showpassword, variable=login_checked, onvalue=1, offvalue=0, cursor='pencil', bg='#DDE6ED')
 
 #add elements to login
 login_lheader.place(relx=0.5,y=45,anchor=CENTER)
@@ -122,10 +129,10 @@ def sign_up():
     writer.writerow(dict)
     csvfile.close()
 
-  main.pack(fill=BOTH, expand=True)
-  x = (screen_width/2) - (500/2)
-  y = (screen_height/2) - (300/2)
-  window.geometry('%dx%d+%d+%d' % (500, 300, x, y))
+  main_frame.pack(fill=BOTH, expand=True)
+  x = (screen_width/2) - (750/2)
+  y = (screen_height/2) - (400/2)
+  window.geometry('%dx%d+%d+%d' % (750, 400, x, y))
 
 def check_user():
     if signup_eusername.get() != '':
@@ -150,30 +157,46 @@ def check_user():
             
     else:
       signup_error.config(text='Please enter a username')
-      
 
 signup_button = ttk.Button(signup, text='Create account', style='button.TButton', command=check_user, cursor='hand2')
 
 #checkbox
-checked = tk.IntVar()
+signup_checked = tk.IntVar()
 
 def signup_showpassword():
-  if checked.get():
+  if signup_checked.get():
     signup_epassword.config(show='')
   else:
     signup_epassword.config(show='*')
 
-signup_checkbox = Checkbutton(signup, command=signup_showpassword, variable=checked, onvalue=1, offvalue=0, cursor='pencil', bg='#DDE6ED')
+signup_checkbox = Checkbutton(signup, command=signup_showpassword, variable=signup_checked, onvalue=1, offvalue=0, cursor='pencil', bg='#DDE6ED')
 
 #add elements to signup
-signup_lheader.place(relx=0.5,y=45,anchor=CENTER)
-signup_lusername.place(x=30,y=70)
-signup_eusername.place(relx=0.5,y=106,anchor=CENTER)
-signup_lpassword.place(x=30,y=130)
-signup_epassword.place(x=30,y=155)
-signup_button.place(relx=0.5,y=215,anchor=CENTER)
+signup_lheader.place(relx=0.5, y=45, anchor=CENTER)
+signup_lusername.place(x=30, y=70)
+signup_eusername.place(relx=0.5, y=106, anchor=CENTER)
+signup_lpassword.place(x=30, y=130)
+signup_epassword.place(x=30, y=155)
+signup_button.place(relx=0.5, y=215, anchor=CENTER)
 signup_checkbox.place(x=240, y=153)
-signup_error.place(relx=0.5,y=250,anchor=CENTER)
+signup_error.place(relx=0.5, y=250, anchor=CENTER)
+##################################################
+
+#MAIN
+##################################################
+main = Frame(main_frame,  bg='#DDE6ED')
+main.pack(fill=BOTH, expand=TRUE)
+
+#buttons
+def open_file():
+  types = (('Audio files', '*.mp3'), ('All files', '*.*'))
+  file = fd.askopenfile(title='Open audio file', filetypes=types)
+
+file_button = ttk.Button(main, text='Open audio file', command=open_file, style='file-button.TButton')
+style.configure('file-button.TButton', )
+
+#add elements to main frame
+file_button.pack()
 ##################################################
 
 window.mainloop()
